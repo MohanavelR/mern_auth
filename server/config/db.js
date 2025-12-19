05c6cd8f74694b95da420e978e8b6497e71ae33a
MohanavelR <rmohanavel2003@gmail.com>
2025-12-18 22:31:34 -0500

    intial

--- server/config/db.js
+++ server/config/db.js
@@ -1,32 +1,10 @@
-// db/connection.js
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const DB_URL = process.env.DB_URL;
-
-if (!DB_URL) {
-  throw new Error("DB_URL not set");
+const setDB_Connection = async()=>{
+ await mongoose.connect(process.env.DB_URL).then(()=>{
+    console.log("DataBase Connected")
+}).catch((error)=>{
+    console.log("Error"+String(error))
+}) 
 }
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function setDB_Connection() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(DB_URL, {
-      bufferCommands: false,
-      serverSelectionTimeoutMS: 10000,
-    });
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
-
-module.exports = setDB_Connection;
+module.exports = setDB_Connection
